1f7750b7badd2aec2c9accf9dc0e2c3ed36006a8
DreamChaser <190401886@qq.com>
2023-11-03 19:23:09 +0800

    12.5.2 _update_screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,21 +12,28 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width,
-                                              self.settings.screen_height))
+                                               self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitime()
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)  # 每秒60帧
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # 更新屏幕上的图像，并切换到新屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitime()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
